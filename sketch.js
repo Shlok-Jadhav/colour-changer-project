8adb9c64bfdd86fb4c8491cd6f2be4bb3c62bc19
Shlok-Jadhav <86878246+Shlok-Jadhav@users.noreply.github.com>
2021-07-08 17:50:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(1500,700);
+  
+}
+
+function draw() {
+
+  if(keyIsDown(RIGHT_ARROW)){
+    background('red');
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    background('blue');
+  }
+  if(keyIsDown(UP_ARROW)){
+    background('yellow');
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    background('green');
+  }
+drawSprites();
+}
+
+
+
+
